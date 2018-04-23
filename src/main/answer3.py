746016268c03c04bc896534be622bff91018dd62
sysuwangwei <xiuqing194@qq.com>
2018-04-23 17:16:11 -0400

    Using arg candidates strategy to answer the WH questions.

--- src/main/answer3.py
+++ src/main/answer3.py
@@ -13,6 +13,7 @@ from questionTypes import WHType
 from questionTypes import BINType
 from questionTypes import detect_type
 from relationRecord import Record
+from relationRecord import keywords_generation
 from graphene_extraction import readRecordDict
 
 TOP_K = 10
@@ -26,79 +27,6 @@ def read_data(file):
     with open(file) as f:
         data = f.readlines()
     return data
-
-# isinstance(obj, WHType)
-def detect_type(sentence):
-	sentence = sentence.lower()
-	for w_type in WHType:
-		if sentence.startswith(w_type.value):
-			return w_type
-
-	for b_type in BINType:
-		if sentence.startswith(b_type.value):
-			return b_type
-
-	return 'Unknown answer type'
-
-
-
-# Generate keywords for quetion
-def keywords_generation(question):
-	r = Rake()
-	r.extract_keywords_from_text(question)
-	keywords = r.get_ranked_phrases()
-	return keywords
-
-print(keywords_generation('Volta was not born in Como.'))
-
-# topK Most Relevant Sentences
-def find_relevant_sentences(keywords, article):
-	article_dict = collections.Counter()
-	for sentence in article:
-		tokens = set()
-		doc = nlp(sentence)
-		for token in doc:
-			tokens.add(str(token))
-		matched_words = set(filter(set(keywords).__contains__, tokens))
-		article_dict[sentence] = len(matched_words)
-
-	relevant_sents = []
-	for (sentence, num_matchwords) in article_dict.most_common(TOP_K):
-		relevant_sents.append(sentence)
-
-	return relevant_sents
-
-def find_answer(keywords, article):
-	article_dict = collections.Counter()
-	for sentence in article:
-		tokens = keywords_generation(sentence)
-		matched_words = set(filter(set(keywords).__contains__, tokens))
-		if (len(tokens) == 0):
-			article_dict[sentence] = 0
-		article_dict[sentence] = len(matched_words) / len(tokens)
-
-	# relevant_sents = []
-	# for (sentence, num_matchwords) in article_dict.most_common(TOP_K):
-	# 	relevant_sents.append(sentence)
-	#
-	#
-	# if article_dict.most_common(1)[0][1] < 0.1:
-	# 	answer = None
-	# answer = article_dict.most_common(1)[0][0]
-	# return answer
-
-# Parse sentence dependency parser
-def extract_answer_by_NER(relevant_sents, ner_list):
-	answers = []
-	for sent in relevant_sents:
-		doc = nlp(sent)
-		# answer = set()
-		for ent in doc.ents:
-			if ent.label_ in ner_list:
-				# answer.add(ent.text)
-				answers.append(ent.text)
-	return answers
-
 
 
 # context, NER, att
@@ -121,60 +49,62 @@ def dealArg(arg):
 	return None, None, None
 
 
-# find an answer from sorted relevant_records
-def answerArg1(question_type, ranked_records):
-	for record in ranked_records:
-		if record.arg1 == '':
-			continue
+# # find an answer from sorted relevant_records
+# def answerArg1(question_type, ranked_records):
+# 	for record, sorce in ranked_records:
+# 		if record.arg1 == '':
+# 			continue
+#
+# 		sub, ner, att = dealArg(record.arg1)
+# 		if question_type == WHType.WHO and ner in ['PERSON']:
+# 			return sub
+# 		elif question_type == WHType.WHOSE and ner in ['PERSON']:
+# 			return sub
+# 	return None
 
-		sub, ner, att = dealArg(record.arg1)
-		if question_type == WHType.WHO and ner in ['PERSON']:
-			return sub
-		elif question_type == WHType.WHOSE and ner in ['PERSON']:
-			return sub
-	return None
 
-def answerArg3(question_type, ranked_records):
-	for record in ranked_records:
-		if record.arg3 == '':
-			continue
 
-		context, ner, _ = dealArg(record.arg3)
-		if question_type == WHType.WHERE and ner in ['LOC']:
-			return context # or return arg3?
-		elif question_type == WHType.WHEN and ner in ['DATE']:
-			return context
-		elif question_type == WHType.WHOM and ner in ['PERSON']: # Need to and this in questionGen
-			return context
-		elif question_type == WHType.HOW:
-			return 'None.something'
-		elif question_type == WHType.HOWLONG:
-			return 'None.something'
-		elif question_type == WHType.HOWOFTEN:
-			return 'None.something'
-	return None
-
-# return score, answer???
-def answerArg123(question_type, ranked_records):
-
-	for record in ranked_records:
-		args = []
-		if record.arg1 != '':
-			args.append(record.arg1)
-		elif record.arg2 != '':
-			args.append(record.arg2)
-		elif record.arg3 != '':
-			args.append(record.arg3)
-		else:
-			continue
-
-		for arg in args:
-			context, ner, att = dealArg(arg)
-			if question_type == WHType.HOWMANY and ner in ['CARDINAL']:
-				return context
-			elif question_type == WHType.WHAT:
-				return context  # or None.something
-	return 'None.something'
+# def answerArg3(question_type, ranked_records):
+# 	for record, score in ranked_records:
+# 		if record.arg3 == '':
+# 			continue
+#
+# 		context, ner, _ = dealArg(record.arg3)
+# 		if question_type == WHType.WHERE and ner in ['LOC']:
+# 			return context # or return arg3?
+# 		elif question_type == WHType.WHEN and ner in ['DATE']:
+# 			return context
+# 		elif question_type == WHType.WHOM and ner in ['PERSON']: # Need to and this in questionGen
+# 			return context
+# 		elif question_type == WHType.HOW:
+# 			return 'None.something'
+# 		elif question_type == WHType.HOWLONG:
+# 			return 'None.something'
+# 		elif question_type == WHType.HOWOFTEN:
+# 			return 'None.something'
+# 	return None
+#
+# # return score, answer???
+# def answerArg123(question_type, ranked_records):
+#
+# 	for record, score in ranked_records:
+# 		args = []
+# 		if record.arg1 != '':
+# 			args.append(record.arg1)
+# 		elif record.arg2 != '':
+# 			args.append(record.arg2)
+# 		elif record.arg3 != '':
+# 			args.append(record.arg3)
+# 		else:
+# 			continue
+#
+# 		for arg in args:
+# 			context, ner, att = dealArg(arg)
+# 			if question_type == WHType.HOWMANY and ner in ['CARDINAL']:
+# 				return context
+# 			elif question_type == WHType.WHAT:
+# 				return context  # or None.something
+# 	return 'None.something'
 
 def test():
 	listRecords = [Record('had been watching', 'Tom', 'the ball', ''),
@@ -190,25 +120,16 @@ def test():
 	print(answerArg3(WHType.WHEN, listRecords))
 
 
-def extract_dobj(relevant_sents):
-	# He has finished his homework.
-	answers = []
-	for sent in relevant_sents:
-		doc = nlp(sent)
-		for doc_sen in doc.sents:
-			for chunk in doc_sen.noun_chunks:
-				if chunk.root.dep_ == 'dobj':
-					answers.append(chunk.text)
-	return answers
 
-def answerWH(question_type, relevant_records):
-	if question_type in [WHType.WHO, WHType.WHOSE, WHType.HOWMANY]:
-		return answerArg1(question_type, relevant_records)
-	elif question_type in [WHType.WHERE, WHType.WHEN]:
-		return answerArg3(question_type, relevant_records)
-	elif question_type in [WHType.WHAT, WHType.HOWMANY]:
-		return answerArg123(question_type, relevant_records)
-	return None
+# def answerWH(question_type, ranked_records):
+# 	if question_type in [WHType.WHO, WHType.WHOSE, WHType.HOWMANY]:
+#
+# 		return answerArg1(question_type, ranked_records)
+# 	elif question_type in [WHType.WHERE, WHType.WHEN]:
+# 		return answerArg3(question_type, ranked_records)
+# 	elif question_type in [WHType.WHAT, WHType.HOWMANY]:
+# 		return answerArg123(question_type, ranked_records)
+# 	return None
 
 
 def extract_answer_by_not(sent, not_list):
@@ -254,25 +175,66 @@ def answerBIN(question_type, relevant_sents):
 	return answer
 
 
+
 def find_relevant_records(dict_records, keywords):
-	relevant_records = []
+	freq_counter = collections.Counter()
 	for rel, list_records in dict_records.items():
 		for record in list_records:
-			if len(set(keywords).intersection(set(record.getKeywords()))) != 0:
-				relevant_records.append(record)
-				record.print_record()
-	return relevant_records
+			sent, recordKeywords = record.getKeywords()
+			matched_words = set(filter(keywords.__contains__, recordKeywords))
+			if len(matched_words) != 0:
+				freq_counter[record] = len(matched_words)
+			# if len(set(keywords).intersection(set(record.getKeywords()))) != 0:
 
+	ranked_records = []
+	for record, num_matchwords in freq_counter.most_common():
+		ranked_records.append((record, num_matchwords))
+	return ranked_records # pair of (record,score)
+
+
+def answerWH(question_type, ranked_records, keywords):
+	for record, score in ranked_records:
+		cands = record.findMissingArg(keywords)
+		for arg in cands:
+			context, ner, att = dealArg(arg)
+			if question_type == WHType.WHO and ner in ['PERSON']:
+				return context
+			elif question_type == WHType.WHOSE and ner in ['PERSON']:
+				return context
+			elif question_type == WHType.WHERE and ner in ['LOC']:
+				return context  # or return arg3?
+
+			elif question_type == WHType.WHEN and ner in ['DATE']:
+				return context
+			elif question_type == WHType.WHOM and ner in ['PERSON']:  # Need to and this in questionGen
+				return context
+			elif question_type == WHType.HOW:
+				return 'None.something'
+			elif question_type == WHType.HOWLONG:
+				return 'None.something'
+			elif question_type == WHType.HOWOFTEN:
+				return 'None.something'
+
+			elif question_type == WHType.HOWMANY and ner in ['CARDINAL']:
+				return context
+			elif question_type == WHType.WHAT:
+				return context  # or None.something
+	return None
 
 #'Who is the presentend?'
 def answer_question(dict_records, question):
 	keywords = keywords_generation(question)
-	relevant_records = find_relevant_records(dict_records, keywords)
+	ranked_records = find_relevant_records(dict_records, keywords)
+	for i in range(10):
+		print(str(ranked_records[i][1]))
+		ranked_records[i][0].print_record()
+
 	question_type = detect_type(question)
 	if isinstance(question_type, WHType):
-		return answerWH(question_type, relevant_records)
+		return answerWH(question_type, ranked_records, keywords)
 	elif isinstance(question_type, BINType):
-		return answerBIN(question_type, relevant_records)
+		return 'Yes.'
+		#return answerBIN(question_type, ranked_records)
 	return None
 
 
@@ -294,11 +256,15 @@ if __name__ == "__main__":
 
 
 	records_file = '../resources/records.txt'
-	question_file = '../resources/question_Alessandro_Volta.txt'
+	# question_file = '../resources/question_Alessandro_Volta.txt'
+	# questions = read_data(question_file)
 
-
-
-	questions = read_data(question_file)
+	questions = ['When did Volta retire?',
+				'Who did Alessandro Volta marry?',
+				 # 'What did Alessandro Volta invent in 1800?',
+				 'When did Alessandro Volta improve  and popularize the electrophorus?',
+				 # 'How long was Alessandro Volta a professor at the University of Pavia?',
+				 'Where was Volta born?']
 
 	dict_records = readRecordDict(records_file)
 
